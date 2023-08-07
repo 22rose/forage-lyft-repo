5c95b968426ca16d64a9ee689c3072225053e34e
Rose Anim <97644788+22rose@users.noreply.github.com>
2023-08-06 22:13:29 -0400

    Create capulet_engine.py

---
+++ parts/Engine/capulet_engine.py
@@ -0,0 +1,16 @@
+from parts.Engine.engine import Engine
+"""Modifying the behavior of a part for all cars which
+contain that part can be accomplished by modifying a
+single piece of code. """
+
+
+class CapuletEngine(Engine):
+    last_service_mileage: int
+    current_mileage: int
+   
+    def __init__(self, last_service_mileage, current_mileage):
+        self.last_service_mileage = last_service_mileage
+        self.current_mileage = current_mileage
+
+    def needs_service(self):
+        return self.current_mileage - self.last_service_mileage >= 30000
